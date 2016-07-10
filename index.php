ef0f990cf5cefad6249a9c068d006e0fe45d4633
Pavlinus <pavlin.kov@mail.ru>
2016-07-10 19:56:26 +0300

    main frame

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ru-ru" dir="ltr">
+
+<head>
+
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="..." />
+  <meta name="keywords" content="..." />
+
+  <link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon"/>
+  <link rel="stylesheet" href="css/normalize.css" type="text/css" />
+  <link rel="stylesheet/less" href="css/mixins.less" type="text/css" />
+  <link rel="stylesheet/less" href="css/style.less" type="text/css" />
+
+  <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.6.1/less.min.js"></script>
+
+  <title>InterBridge</title>
+
+  <!--script
+  src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" defer>
+</script>
+<script src="https://code.jquery.com/ui/1.11.3/jquery-ui.min.js" defer>
+</script-->
+
+</head>
+
+<body>
+
+  <header>
+
+    <div class="container">
+      <div class="logo">Logo</div><div class="top_image">Top image</div>
+    </div>
+
+    <nav>
+      <ul>
+        <li><a href="">text1</a></li>
+        <li><a href="">text2</a></li>
+        <li><a href="">text3</a></li>
+      </ul>
+    </nav>
+
+  </header>
+
+  <div class="main">
+    <div class="container">
+
+      <div class="left_panel">
+        left_panel
+      </div>
+      <div class="content">
+        content
+      </div>
+      <div class="right_panel">
+        right_panel
+      </div>
+
+    </div>
+  </div>
+
+
+  <footer>
+    <div class="container">
+
+    </div>
+  </footer>
+
+</body>
+</html>
